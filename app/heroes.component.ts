43a3bfb1bd156f57a2f2df3a1f1314cca9773b16
adityaanup <emailadityaanup@gmail.com>
2016-11-29 12:50:44 +0530

    Restructuring and styling app

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
 
 @Component({
+  moduleId: module.id,
   selector: 'my-heroes',
   providers: [HeroService],
-  templateUrl:'heroes.component.html',
-  styleUrls:['heroes.component.css']
+  templateUrl: 'heroes.component.html',
+  styleUrls: ['heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService){}
+  constructor(
+    private router : Router,
+    private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
@@ -25,5 +31,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
